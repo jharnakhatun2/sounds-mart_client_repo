fac186d3d05f181696ddd0010944eba374b6377f
Jharna203 <jharnakhatun203@gmail.com>
2022-11-24 06:38:07 +0600

    added route setup and navbar

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,28 @@
+import {createBrowserRouter} from 'react-router-dom';
+import Login from '../../Authentication/Login/Login';
+import Signup from '../../Authentication/Signup/Signup';
+import Main from '../../Layout/Main';
+import Home from '../../Pages/Home/Home/Home';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+])
+
+export default router;
